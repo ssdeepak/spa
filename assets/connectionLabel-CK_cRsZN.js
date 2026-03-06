5c2b4dc60e37bcd29cdbb59e2c36c02119f0805f
ssdeepak <ssdeepak@users.noreply.github.com>
2026-03-06 14:53:23 +0000

    deploy: update landing page ssdeepak/enterprise@2a8f7ce83ec06c785ead065c95058d48b866bf86

---
+++ assets/connectionLabel-CK_cRsZN.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./index-DXxtNZ7_.js";var o=e();function t(a){return a.displayName||a.alias||a.connectionId||"Unnamed Connection"}export{t as g,o as r};
--- assets/index-C6QmyNWQ.js
+++
@@ -1,1 +0,0 @@
-import{ae as r}from"./index-PpYIyNMM.js";var e=r();export{e as r};
